dcd616a4f252f326d541d3c89901c3953fb4a6b2
MairaGeorgina <mairageorgina@gmail.com>
2016-04-25 23:11:01 -0300

    archivo bootstrap actualizado para cargar roles

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
+import vermax.Rol
+
 class BootStrap {
 
     def init = { servletContext ->
+		
+		def rol_admin = Rol.findByNombre("ADMIN")
+		if (!rol_admin) {
+			rol_admin = new Rol(nombre: "ADMIN", descripcion: "Administracion")
+			rol_admin.save()
+		}
+		
+		def rol_super = Rol.findByNombre("SUPER")
+		if (!rol_super) {
+			rol_super = new Rol(nombre: "SUPER", descripcion: "Super Usuario")
+			rol_super.save()
+		}
     }
-    def destroy = {
+
+	    def destroy = {
     }
 }

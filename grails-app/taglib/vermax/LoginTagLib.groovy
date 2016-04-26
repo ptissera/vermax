f4053feed11fb32a14c1e03fc6746344b4b6d8d7
Pablo Tissera <tissera.pablo@gmail.com>
2016-04-25 21:34:16 -0300

    add customs taglib

---
+++ grails-app/taglib/vermax/LoginTagLib.groovy
@@ -0,0 +1,25 @@
+package vermax
+
+import grails.artefact.Artefact;
+
+@Artefact("TagLibrary")
+class LoginTagLib {
+	static namespace = "pulko"
+	static encodeAsForTags = [tagName: [taglib:'raw']]
+	
+	def loginControl = { attrs, body ->
+		out << "<div id='login'>"
+		if(session.usuario){			
+            out << "<label>"
+			out << " ${session.usuario} "                        
+			out << "</label>"
+			out << """${link(action:"logout", controller:"authorize"){"<b>Logout</b>"}}"""
+			out << "</div>"
+		} else{
+			out << """${link(action:"openSearch", controller:"asistencia"){"<b>Asistencia</b>"}}"""
+			out << """${link(action:"login", controller:"authorize"){"<b>Login</b>"}}"""			
+		} 		
+		out << "</div>"
+		
+	}
+}
